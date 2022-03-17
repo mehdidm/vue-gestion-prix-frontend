0a0a6551af0b5a89e9bd0fcad96fc5c3f182bd8d
Mehdi <dimassimehdi58@gmail.com>
2022-03-17 22:03:10 +0100

    wip:update price

--- src/Listing/ListingProducts.vue
+++ src/Listing/ListingProducts.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="tab1">
+    <button type="button" style="margin: 10px" class="btn btn-dark">
+      Exporter fichier
+    </button>
     <table class="table table-hover product-table mx-auto">
       <thead>
         <tr>
@@ -13,7 +16,7 @@
       <tbody>
         <tr v-for="product in products" :key="product" track-by="id">
           <td>{{ product.id }}</td>
-          <td><img :src="apiImage+product.media"  class="imgProduct"/></td>
+          <td><img :src="apiImage + product.media" class="imgProduct" /></td>
           <td>{{ product.name }}</td>
           <td>{{ product.price }}</td>
           <td>
@@ -30,13 +33,13 @@
         </tr>
         <!-- Modal -->
         <div
-          class="modal fade "
+          class="modal fade"
           id="exampleModal"
           tabindex="-1"
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog" style="max-width: 70%;" role="document">
+          <div class="modal-dialog" style="max-width: 70%" role="document">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title styleTitleModal" id="exampleModalLabel">
@@ -49,19 +52,24 @@
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="styleModal" >
-                <img :src="apiImage+selectedUser.media" class="imgProduct"/>
-              
+              <div class="styleModal">
+                <img :src="apiImage + selectedUser.media" class="imgProduct" />
+
                 <h3 class="styleTextModal">Catégorie :</h3>
-                <p class="styleTextModal">{{ selectedUser.category }}</p >
+                <p class="styleTextModal">{{ selectedUser.category }}</p>
                 <h3 class="styleTextModal">Détails points :</h3>
-                <p class="styleTextModal">{{ selectedUser.detailPoints }}</p >
+                <p class="styleTextModal">{{ selectedUser.detailPoints }}</p>
                 <h3 class="styleTextModal">Détails :</h3>
-                <p class="styleTextModal">{{ selectedUser.details }}</p >
+                <p class="styleTextModal">{{ selectedUser.details }}</p>
                 <h3 class="styleTextModal">Prix :</h3>
-                <p class="styleTextModal">{{ selectedUser.price }}</p >
-                <br/>
-                <input v-model="price_provider" placeholder="Saisie le prix provider">
+                <p class="styleTextModal">{{ selectedUser.price }}</p>
+                <br />
+               <h1> Saisie prix</h1>
+                <input
+                  type="number"
+                  v-model="price_provider"
+                  placeholder="Saisie le prix provider"
+                />
               </div>
               <div class="modal-footer">
                 <button
@@ -71,8 +79,12 @@
                 >
                   Close
                 </button>
-                <button type="button" @click="updatePriceProvider(selectedUser.id,price_provider)" class="btn btn-primary">
-                 Enregistrer
+                <button
+                  type="button"
+                  @click="updatePriceProvider(selectedUser.id)"
+                  class="btn btn-primary"
+                >
+                  Enregistrer
                 </button>
               </div>
             </div>
@@ -87,10 +99,10 @@ import axios from "axios";
 export default {
   data() {
     return {
-      apiImage : "http://37.187.244.116:5000/static/",
+      apiImage: "http://37.187.244.116:5000/static/",
       products: [],
       selectedUser: "",
-      price_provider:"",
+      price_provider:0,
     };
   },
   mounted() {
@@ -110,18 +122,37 @@ export default {
         )
         .then((response) => (this.products = response.data.products));
     },
+    export() {
+      axios.get(
+        "http://37.187.244.116:5000/export/" + this.$route.params.category,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json",
+          },
+        }
+      );
+    },
     sendInfo(item) {
       this.selectedUser = item;
     },
-    updatePriceProvider(id,price_provider){
-        axios.post("http://37.187.244.116:5000/product/"+id, price_provider,{
-           headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Content-type": "application/json",
-            },
+    updatePriceProvider(id) {
+   
+   const json = JSON.stringify({ "price_provider": this.price_provider });
+
+      axios
+        .put("http://37.187.244.116:5000/product/" + id,json, {
+          headers: {
+       'Content-Type': 'application/json'
+          },
         })
-    .then(response => console.log('ici',response));
-    }
+        .then((response) =>
+          this.$toast("My toast content"+response.data, {
+            timeout: 2000,
+          })
+           
+        );
+    },
   },
 };
 </script>
@@ -129,24 +160,19 @@ export default {
 .tab {
   margin: 25%;
 }
-.imgProduct{
-  width : 60%;
+.imgProduct {
+  width: 20%;
 }
-.styleTextModal{
+.styleTextModal {
+
+  text-align: left;
+  margin-left: 5px;
+}
+.modalsize {
  
-  width: 100%;
-  text-align: left;
-  margin-left:5px;
-}
-.modalsize{
-  
-  width: 120%;
   margin: auto;
-
 }
-.styleTitleModal{
-  
+.styleTitleModal {
   text-align: left;
-
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './router'
 
-Vue.config.productionTip = false
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
+Vue.config.productionTip = false
+Vue.use(Toast);
 new Vue({
   router,
+  
   render: h => h(App)
 }).$mount('#app')
